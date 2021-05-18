efbf5f3ac7b3b31c4ff108921e1f49bb3e2683cc
eternal-moon <anishapant1@gmail.com>
2021-05-18 14:53:52 +0545

    add files complete

--- HTML5 Canvas/index.js
+++ HTML5 Canvas/index.js
@@ -10,21 +10,43 @@ ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 100;
+//ctx.globalCompositeOperation = 'multiply'; //blends the overlap
 
 //when we don't click anything and just moving mouse around -- doesn't draw
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue=0;
+let direction = true;
 
 function draw(e){
     if(!isDrawing) return; //stop the function from running when they are not moused down
     console.log(e);
+    ctx.strokeStyle =`hsl(${hue}, 100%, 50%)`;
+    
     ctx.beginPath();
     ctx.moveTo(lastX, lastY); //start from
     ctx.lineTo(e.offsetX, e.offsetY); //go to
     ctx.stroke();
     [lastX, lastY] = [e.offsetX, e.offsetY]; //updating position -- one line(destructuring an array)
+    //changing colors
+    hue++;
+    if(hue>=360){
+        hue=0;
+
+    }
+
+    //changing the width of line
+    if(ctx.lineWidth >=100 || ctx.lineWidth <=1){
+        direction =!direction;
+    }
+    if(direction){
+        ctx.lineWidth++;
+    } else{
+        ctx.lineWidth--;
+    }
 }
+
 
 
 canvas.addEventListener('mousedown', (e) => {
